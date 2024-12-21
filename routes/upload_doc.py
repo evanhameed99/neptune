eef9ca19a487ad8ae1ca42aa2e27230e98874840
evanhameed99 <evanhameed9966@gmail.com>
2024-12-22 00:02:25 +0300

    Remove unnecessary comments

--- routes/upload_doc.py
+++ routes/upload_doc.py
@@ -16,17 +16,15 @@ async def upload_doc(
     file: UploadFile = Depends(ensure_pdf_file_upload_dep),
 ):
     try:
-        # Generate unique upload ID and file paths
+
         upload_id = str(uuid.uuid4())
         os.makedirs(PDF_DIR, exist_ok=True)
         file_path = os.path.join(PDF_DIR, f"{upload_id}.pdf")
 
-        # Save the uploaded file asynchronously
         with open(file_path, "wb") as f:
-            while chunk := await file.read(1024 * 1024):  # Read in 1MB chunks
+            while chunk := await file.read(1024 * 1024):
                 f.write(chunk)
 
-        # Add background task to process PDF
         background_tasks.add_task(DocTextExtractor.save_doc_markdown, upload_id)
         background_tasks.add_task(MarkdownSplitter.locate_and_split_markdown, upload_id)
 
